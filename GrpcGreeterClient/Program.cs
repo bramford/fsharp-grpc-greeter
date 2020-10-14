ec6b64213c7a7cf7046f995a081cc4578d5a0f46
Bramford Horton <bram.horton@derp.tech>
2020-10-14 15:23:25 +1300

    Disable TLS verify for dev self-signed cert

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -9,10 +9,16 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
-            // The port number(5001) must match the port of the gRPC server.
-            var co = new GrpcChannelOptions();
-            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
-            var client =  new Greeter.GreeterClient(channel);
+            // Custom HttpClientHandler that ignores TLS server cert
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            var httpClient = new HttpClient(httpClientHandler);
+            httpClient.DefaultRequestVersion = new Version(2, 0);
+            var options = new GrpcChannelOptions();
+            options.HttpClient = httpClient;
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001", options);
+            var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
