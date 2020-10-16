c59a7712b9fb00391ca00bf48f686e51eb79ff8c
Bramford Horton <bram.horton@derp.tech>
2020-10-16 22:04:12 +1300

    Move proto definition and grpc layer to library

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using GrpcGreeterLib;
 
 namespace GrpcGreeterClient
 {
---
+++ GrpcGreeterLib/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GrpcGreeterLib
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("This program should not be run.");
+            return 1;
+        }
+    }
+}
